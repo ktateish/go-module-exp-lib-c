c21cdb4e2d1573c7199abffced1e8a37c1fdeb67
Katsuyuki Tateishi <kt@wheel.jp>
2019-09-15 10:24:55 +0900

    Add log in test

--- function_test.go
+++ function_test.go
@@ -1,6 +1,7 @@
 package c
 
 import (
+	"log"
 	"strings"
 	"testing"
 )
@@ -8,6 +9,7 @@ import (
 func TestF(t *testing.T) {
 	s := "foo"
 	x := F(s)
+	log.Println(x)
 	if !strings.HasPrefix(x, "C v") {
 		t.Errorf("errornous prefix")
 	}
